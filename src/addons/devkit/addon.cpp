d7624666896fc60e7f64defbcb6aa72cb887bab3
Mohanned Hassan <gailardiagalane@gmail.com>
2025-12-29 20:47:01 +0400

    feat(devkit): Add slang support (Using Vulkan SDK)

--- src/addons/devkit/addon.cpp
+++ src/addons/devkit/addon.cpp
@@ -47,6 +47,7 @@
 #include "../../utils/pipeline_layout.hpp"
 #include "../../utils/shader.hpp"
 #include "../../utils/shader_compiler_directx.hpp"
+#include "../../utils/shader_compiler_slang.hpp"
 #include "../../utils/shader_compiler_watcher.hpp"
 #include "../../utils/shader_decompiler_dxc.hpp"
 #include "../../utils/shader_dump.hpp"
@@ -362,6 +363,8 @@ bool ComputeDisassemblyForShaderDetails(reshade::api::device* device, DeviceData
         shader_details->disassembly = std::string(
             shader_details->shader_data.data(),
             shader_details->shader_data.data() + shader_details->shader_data.size());
+      } else if (device->get_api() == reshade::api::device_api::vulkan) {
+        shader_details->disassembly = renodx::utils::shader::compiler::slang::DisassembleSpirv(shader_details->shader_data, shader_details->shader_hash);
       } else {
         throw std::exception("Unsupported device API.");
       }
@@ -5012,7 +5015,9 @@ void RenderShaderViewLive(reshade::api::device* device, DeviceData* data, Shader
   if (shader_details->disk_shader.has_value()) {
     if (!shader_details->disk_shader->IsCompilationOK()) {
       live_string = shader_details->disk_shader->GetCompilationException().what();
-    } else if (shader_details->disk_shader->is_hlsl || shader_details->disk_shader->is_glsl) {
+    } else if (shader_details->disk_shader->is_hlsl
+               || shader_details->disk_shader->is_glsl
+               || shader_details->disk_shader->is_slang) {
       try {
         live_string = renodx::utils::path::ReadTextFile(shader_details->disk_shader->file_path);
       } catch (std::exception& e) {
@@ -5071,6 +5076,10 @@ void RenderShaderViewDecompilation(reshade::api::device* device, DeviceData* dat
             {
                 .flatten = true,
             });
+      } else if (device->get_api() == reshade::api::device_api::vulkan) {
+        shader_details->decompilation = renodx::utils::shader::compiler::slang::DecompileSpirvToGlsl(
+            shader_details->shader_data,
+            shader_details->shader_hash);
       } else if (device->get_api() == reshade::api::device_api::opengl) {
         shader_details->disassembly = std::string(
             shader_details->shader_data.data(),
